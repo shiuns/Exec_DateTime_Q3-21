b6366099350c3ed1bb460617187d0bf7177ea725
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 12:11:49 +0800

    加入專案檔案。

---
+++ [(Exec_DateTime_Q3)-21 謝孟勳/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Exec_DateTime_Q3__21_謝孟勳
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            int year = today.Year;
+            int month = today.Month;
+
+            DateTime firstday = new DateTime(year, month, 1);
+            Console.WriteLine(firstday);
+        }
+    }
+}
